bf2258e0ec4871208d7f59c8514bdabca2233a3f
Daniel Taylor <dptaylor98@gmail.com>
2022-01-15 01:23:47 -0700

    set on/off label on page load

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const loadTheme = function() {
 	if (localStorage.getItem('theme') == 'light') {
 		document.documentElement.setAttribute('data-theme', 'light');
 		$('#toggle-lights').toggleClass('fa-toggle-off fa-toggle-on');
+		$('#onoff').text('off');
 	}
 }
